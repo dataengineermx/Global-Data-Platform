6239916a71fff4ef91d320829c1111d6975c6a5d
dataengineermx <adrian.valentino@outlook.com>
2026-07-01 17:10:20 +0000

    s

--- Pipeline Multi-Cloud/src/01_load_to_csv_population.py
+++ Pipeline Multi-Cloud/src/01_load_to_csv_population.py
@@ -3,18 +3,18 @@ import logging
 from paths import data_raw_path
 from extract_api_population import df
 
+logging.basicConfig(
+level=logging.INFO,
+format='%(asctime)s | %(levelname)s | %(message)s')
 
+logging.info("Load started")
 
 try:
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s | %(levelname)s | %(message)s'
-    )
-    logging.info("Load started")
-
+    
     with df.to_csv(data_raw_path/"population.csv", index=False) as f:
 
         logging.info("File loaded successfully")
+
 except Exception as e:
     print(type(e).__name__, e)
     logging.error("Error when loading records")
