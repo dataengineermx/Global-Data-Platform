ff0beda96841127e5689bb2ea8fb9a40baef1ce5
dataengineermx <adrian.valentino@outlook.com>
2026-07-01 20:27:20 +0000

    s

--- Pipeline Multi-Cloud/src/01_load_to_csv_population.py
+++ Pipeline Multi-Cloud/src/01_load_to_csv_population.py
@@ -4,6 +4,7 @@ from paths import data_raw_path
 from extract_api_population import df
 from logging_config import logging
 from logging_config import config_my_logger
+from logging_config import obtener_resumen
 
 logger = config_my_logger("Job 01_load_population")
 logger.info("Job Started")
@@ -11,6 +12,7 @@ try:
     
     df.to_csv(data_raw_path/"population.csv", index=False)
     logging.info("File loaded successfully")
+    obtener_resumen("Resumen")
 
 except Exception as e:
     print(type(e).__name__, e)
