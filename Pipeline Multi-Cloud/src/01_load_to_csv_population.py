1138299fa83585a68dea15a44ee7b44a8af9c581
dataengineermx <adrian.valentino@outlook.com>
2026-07-01 17:20:41 +0000

    s

--- Pipeline Multi-Cloud/src/01_load_to_csv_population.py
+++ Pipeline Multi-Cloud/src/01_load_to_csv_population.py
@@ -3,17 +3,27 @@ import logging
 from paths import data_raw_path
 from extract_api_population import df
 
+#Execution Controls
 logging.basicConfig(
 level=logging.INFO,
 format='%(asctime)s | %(levelname)s | %(message)s')
 
-logging.info("Load started")
+total_leidos = 0
+total_insertados = 0
+total_rechazados = 0
+
+
+logging.info("Load started"),
+logging.info(
+    f"Leídos={total_leidos}, "
+    f"Insertados={total_insertados}, "
+    f"Rechazados={total_rechazados}"             
+             )
 
 try:
     
-    with df.to_csv(data_raw_path/"population.csv", index=False) as f:
-
-        logging.info("File loaded successfully")
+    df.to_csv(data_raw_path/"population.csv", index=False)
+    logging.info("File loaded successfully")
 
 except Exception as e:
     print(type(e).__name__, e)
