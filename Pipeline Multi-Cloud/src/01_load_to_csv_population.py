4e4bf0d6d9ef152f6deabc794353ab1796b2ead4
dataengineermx <adrian.valentino@outlook.com>
2026-07-01 20:25:10 +0000

    s

--- Pipeline Multi-Cloud/src/01_load_to_csv_population.py
+++ Pipeline Multi-Cloud/src/01_load_to_csv_population.py
@@ -2,9 +2,11 @@ import pandas as pd
 import logging
 from paths import data_raw_path
 from extract_api_population import df
+from logging_config import logging
+from logging_config import config_my_logger
 
-
-
+logger = config_my_logger("Job 01_load_population")
+logger.info("Job Started")
 try:
     
     df.to_csv(data_raw_path/"population.csv", index=False)
