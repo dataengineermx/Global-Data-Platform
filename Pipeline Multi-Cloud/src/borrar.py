4b8cf35252aaad9da2425859dcdf1cf1e8e8fbc4
dataengineermx <adrian.valentino@outlook.com>
2026-07-01 20:02:29 +0000

    s

--- Pipeline Multi-Cloud/src/borrar.py
+++ Pipeline Multi-Cloud/src/borrar.py
@@ -1,6 +1,10 @@
 from logging_config import config_my_logger
+from logging_config import handle
+from logging_config import obtener_resumen
+from logging_config import configurar_mi_logger
 
 logger = config_my_logger("JOB_001")
 
 logger.info("Inicio del proceso")
 logger.error("Error en la carga")
+handle.self("Contador")
