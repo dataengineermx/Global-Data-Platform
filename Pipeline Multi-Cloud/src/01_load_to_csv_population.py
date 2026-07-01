d3f376655367b7c5e5bbd781fa5927cbe4bb8e90
dataengineermx <adrian.valentino@outlook.com>
2026-07-01 17:02:23 +0000

    s

--- Pipeline Multi-Cloud/src/01_load_to_csv_population.py
+++ Pipeline Multi-Cloud/src/01_load_to_csv_population.py
@@ -4,13 +4,17 @@ from paths import data_raw_path
 from extract_api_population import df
 
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s | %(levelname)s | %(message)s'
-)
 
-logging.info("Inicio de carga")
-#logging.info("Archivo leído correctamente")
-#logging.error("Error al insertar registros")
+try:
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s | %(levelname)s | %(message)s'
+    )
+    logging.info("Load started")
 
-df.to_csv(data_raw_path/"population.csv", index=False)
+    with df.to_csv(data_raw_path/"population.csv", index=False) as f:
+
+        logging.info("File loaded successfully")
+except Exception as e:
+    print(type(e).__name__, e)
+    logging.error("Error when loading records")
