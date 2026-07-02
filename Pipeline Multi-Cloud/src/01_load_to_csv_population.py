457f316f58e668c16ded7905f2ea8cd65febbf6f
dataengineermx <adrian.valentino@outlook.com>
2026-07-02 16:18:55 +0000

    S

--- Pipeline Multi-Cloud/src/01_load_to_csv_population.py
+++ Pipeline Multi-Cloud/src/01_load_to_csv_population.py
@@ -1,17 +1,22 @@
-import time
+from datetime import datetime
 from paths import data_raw_path
 from extract_api_population import df
 from job_monitor import LoadMonitor
-from logging_config import get_logger
+
+from zoneinfo import ZoneInfo
+
+
 
 job_monitor = LoadMonitor("01_load_to_csv_population")
 job_monitor.start()
+
 
 try:
     records = len(df)
     df.to_csv(data_raw_path/"population.csv", index=False)
     
     job_monitor.success(records)
+    job_monitor.get_logger()
 
-except Exception as e:
-    job_monitor.failed(e)
+except Exception as error:
+    job_monitor.failed(error)
--- Pipeline Multi-Cloud/src/job_monitor.py
+++ Pipeline Multi-Cloud/src/job_monitor.py
@@ -1,6 +1,8 @@
 import logging
 import uuid
 from datetime import datetime
+from zoneinfo import ZoneInfo
+from pathlib import Path
 
 logging.basicConfig(
     level=logging.INFO,
@@ -14,9 +16,10 @@ class LoadMonitor:
         self.run_id = str(uuid.uuid4())[:8]
         self.process_name = process_name
         self.start_time = None
+        self.logger = self.get_logger()
 
     def start(self):
-        self.start_time = datetime.now()
+        self.start_time = datetime.now(ZoneInfo("America/Mexico_City"))
 
         logging.info(
             f"RUN_ID={self.run_id} | "
@@ -25,7 +28,7 @@ class LoadMonitor:
         )
 
     def success(self, records):
-        end_time = datetime.now()
+        end_time = datetime.now(ZoneInfo("America/Mexico_City"))
 
         duration = (
             end_time - self.start_time
@@ -40,7 +43,7 @@ class LoadMonitor:
         )
 
     def failed(self, error):
-        end_time = datetime.now()
+        end_time = datetime.now(ZoneInfo("America/Mexico_City"))
 
         duration = (
             end_time - self.start_time
@@ -54,3 +57,28 @@ class LoadMonitor:
             f"MESSAGE={error} | "
             f"DURATION={duration:.2f}s"
         )
+
+    def get_logger(self):
+
+        # Crear carpeta logs si no existe
+        Path("logs").mkdir(exist_ok=True)
+        logger = logging.getLogger(self.process_name)
+        logger.setLevel(logging.INFO)
+
+        # Evita agregar handlers duplicados
+        if not logger.handlers:
+
+            formatter = logging.Formatter(
+                '%(asctime)s | %(levelname)s | %(name)s | %(message)s'
+            )
+
+            file_handler = logging.FileHandler(
+                f"logs/{self.process_name}.log",
+                encoding="utf-8"
+            )
+
+            file_handler.setFormatter(formatter)
+
+            logger.addHandler(file_handler)
+
+        return logger
