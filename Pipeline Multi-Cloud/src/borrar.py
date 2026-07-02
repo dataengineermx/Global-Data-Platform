1011559fd4df028608bfcd149b993dcb8b3e363b
dataengineermx <adrian.valentino@outlook.com>
2026-07-02 14:50:41 +0000

    a

--- Pipeline Multi-Cloud/src/borrar.py
+++
@@ -1,7 +0,0 @@
-from logging_config import config_my_logger
-from logging_config import elapsedtime
-logger = config_my_logger("JOB_001")
-
-logger.info("Inicio del proceso")
-#logger.error("Error en la carga")
-elapsedtime
