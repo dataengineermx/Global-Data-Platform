78182d27479ca2f64b284b5c0fafabcdbe0e6c8c
dataengineermx <adrian.valentino@outlook.com>
2026-07-03 18:37:48 +0000

    u

--- Pipeline Multi-Cloud/src/loading/load_to_csv_earthquakes.py
+++ Pipeline Multi-Cloud/src/loading/load_to_csv_earthquakes.py
@@ -1,16 +1,20 @@
-import pandas as pd
 from src.utils.paths import data_raw_path
 from src.extraction.extract_api_earthquakes import df
 from src.monitor.job_monitor import LoadMonitor
 
-job_monitor = LoadMonitor("load_to_csv_earthquakes")
-job_monitor.start()
 
-try:
+def main():  #This prevents the code from executing accidentally when the module is imported by another module
+    job_monitor = LoadMonitor("load_to_csv_earthquakes")
+    job_monitor.start()
 
-    records = len(df)
-    df.to_csv(data_raw_path/"earthquakes.csv", index=False)
-    job_monitor.success(records)
+    try:
 
-except Exception as error:
-    job_monitor.failed(error)
+        records = len(df)
+        df.to_csv(data_raw_path/"earthquakes.csv", index=False)
+        job_monitor.success(records)
+
+    except Exception as error:
+        job_monitor.failed(error)
+
+if __name__ == "__main__":
+    main()
--- Pipeline Multi-Cloud/src/loading/load_to_csv_population.py
+++ Pipeline Multi-Cloud/src/loading/load_to_csv_population.py
@@ -1,19 +1,18 @@
-#from datetime import datetime
 from src.utils.paths import data_raw_path
 from src.extraction.extract_api_population import df
 from src.monitor.job_monitor import LoadMonitor
 
+def main():  #This prevents the code from executing accidentally when the module is imported by another module
+    job_monitor = LoadMonitor("load_to_csv_population")
+    job_monitor.start()
 
+    try:
+        records = len(df)
+        df.to_csv(data_raw_path/"population.csv", index=False)
+        job_monitor.success(records)
 
-job_monitor = LoadMonitor("load_to_csv_population")
-job_monitor.start()
+    except Exception as error:
+        job_monitor.failed(error)
 
-
-try:
-    records = len(df)
-    df.to_csv(data_raw_path/"population.csv", index=False)
-    
-    job_monitor.success(records)
-
-except Exception as error:
-    job_monitor.failed(error)
+if __name__ == "__main__":
+    main()
